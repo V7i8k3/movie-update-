cb1edc442510e26df4270afffa630b3e3b08ed9f
Faheem786210254 <144519272+Faheem786210254@users.noreply.github.com>
2023-10-20 21:30:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -98,9 +98,9 @@ URL = "https://lunabotb-f5e417441bf8.herokuapp.com".format(FQDN) if ON_HEROKU or
     "https://lunabotb-f5e417441bf8.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'SMARTBOT'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'lunabotb'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'smartfilterbot'))
+name = str(environ.get('name', 'lunabotb'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
